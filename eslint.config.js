8402c7737e1c033893d885bdef8044bf35dd6d0a
Sebastian Boehler <basti.boehler@hotmail.de>
2024-09-13 16:10:39 +0800

    chore: fix eslint workflow error

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import { ESLint } from 'eslint';
+
+export default [
+  {
+    ignores: ['node_modules/**', 'dist/**'],
+  },
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      ecmaVersion: 2018,
+      sourceType: 'module',
+      parser: '@typescript-eslint/parser',
+    },
+    plugins: {
+      '@typescript-eslint': '@typescript-eslint/eslint-plugin',
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+  },
+];
